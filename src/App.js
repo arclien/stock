ad22c4dc6fd03217eb39326ada9e6f42add2073d
Jisuk Yoon <arclien>
2020-08-27 19:03:26 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import krx from "krx-stock-api";
+import "./App.css";
 
 function App() {
+  const [stock, setStock] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await krx.getStock("005930");
+      console.log(res);
+      setStock(res);
+    };
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
+      {res}
     </div>
   );
 }
