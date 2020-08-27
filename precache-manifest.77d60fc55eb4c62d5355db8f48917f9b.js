2bc2d902add41b39288141b2a615ec131f40658b
Jisuk Yoon <arclien>
2020-08-27 19:07:03 +0900

    Updates

---
+++ precache-manifest.77d60fc55eb4c62d5355db8f48917f9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a322ffa6a30bb588f7e139eceae23cda",
+    "url": "/stock/index.html"
+  },
+  {
+    "revision": "cae2b5e86749e2a80dde",
+    "url": "/stock/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f01eeb11f85b6b05b6bc",
+    "url": "/stock/static/js/2.1dbf5995.chunk.js"
+  },
+  {
+    "revision": "119fd09ca82a2d4b566f46ad2e6b29c2",
+    "url": "/stock/static/js/2.1dbf5995.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae2b5e86749e2a80dde",
+    "url": "/stock/static/js/main.a3069474.chunk.js"
+  },
+  {
+    "revision": "cf4916082c6581fc77aa",
+    "url": "/stock/static/js/runtime-main.beb22cf1.js"
+  }
+]);
