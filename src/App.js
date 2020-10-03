118a09b4b389f1ade12566e47fb59b639d73e855
Jisuk Yoon <arclien>
2020-10-03 22:35:18 +0900

    add multi series

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from "react";
 import ReactEcharts from "echarts-for-react";
 
-import { readString, CSVReader } from "react-papaparse";
-import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import { readString } from "react-papaparse";
 import "react-tabs/style/react-tabs.css";
 
 import "./App.css";
 
+const stock_list = ["005930", "066570", "005490", "000720", "068760"];
 const getOptions = () => ({
   tooltip: {
     trigger: "axis",
@@ -14,9 +14,10 @@ const getOptions = () => ({
       type: "cross",
       animation: false,
     },
-    formatter: function (params) {
-      return `날짜 : ${params[0].name} <br> 가격 : ${params[0].value}`;
-    },
+    // formatter: function (params) {
+    //   console.log(params);
+    //   return `날짜 : ${params[0].name} <br> 가격 : ${params[0].value} <br> 종목 :  ${params[0].seriesName}`;
+    // },
   },
   xAxis: {
     type: "category",
@@ -27,104 +28,107 @@ const getOptions = () => ({
     axisLabel: {
       formatter: "{value} 원",
     },
-    min: 0,
-    max: 0,
-    interval: 0,
+    // min: 0,
+    // max: 0,
+    // interval: 0,
   },
-  series: [
-    {
-      data: [],
-      type: "line",
-    },
-  ],
+  series: [],
 });
 
 function App() {
   const [stock, setStock] = useState(null);
-  const [dailyPrices, setDailyPrices] = useState([]);
+  const [isLoaded, setLoaded] = useState(false);
   const [option, setOption] = useState(getOptions());
-  const [tabIndex, setTabIndex] = useState(0);
-  const [chartInstance, setChartInstance] = useState(null);
 
-  const aa = (index) => {
-    setTabIndex(index);
-  };
   useEffect(() => {
-    async function getData() {
-      console.log(chartInstance);
+    async function fetchDataFromCsv(stockNumber) {
+      const response = await fetch(
+        `${window.location.origin}/stock/data/${stockNumber}.csv`
+      );
+      const reader = response.body.getReader();
+      const result = await reader.read(); // raw array
+      const decoder = new TextDecoder("utf-8");
+      const csv = decoder.decode(result.value); // the csv text
+      return readString(csv);
+    }
+
+    function getData() {
+      // console.log(chartInstance);
 
       // if (chartInstance) {
       //   chartInstance.dispose();
       // }
 
       // if (chartInstance) chartInstance.getEchartsInstance().clear();
-      const response = await fetch(
-        `${window.location.origin}/stock/data/000720.csv`
-      );
-      const reader = response.body.getReader();
-      const result = await reader.read(); // raw array
-      const decoder = new TextDecoder("utf-8");
-      const csv = decoder.decode(result.value); // the csv text
-      const results = readString(csv);
-      console.log("---------------------------");
-      console.log(results);
-      console.log("---------------------------");
-      setDailyPrices(results.data);
 
-      const length = results.data.length;
-      const data = results.data
-        .slice(1, length - 1)
-        .map((el, i) => parseInt(el[4], 10));
-      const interval = Math.floor(
-        (Math.max(...data) - Math.min(...data) + 10000) / 5
-      );
-      console.log(option);
-      setOption({
+      const stockData = {
         ...option,
         xAxis: {
           ...option.xAxis,
-          data: results.data.slice(1, length - 1).map((el) => el[0]),
         },
         yAxis: {
           ...option.yAxis,
-          min: Math.min(...data) - interval,
-          max: Math.max(...data) + interval,
-          interval: interval,
         },
-        series: {
-          ...option.series,
-          data: results.data
+        series: [...option.series],
+      };
+
+      const fetchAllData = [];
+      stock_list.forEach(async (number) => {
+        fetchAllData.push(fetchDataFromCsv(number));
+      });
+
+      Promise.all(fetchAllData).then((data) => {
+        let minValue;
+        let maxValue;
+        let interval;
+        data.forEach(({ data: stock }, index) => {
+          const length = stock.length;
+          const priceList = stock
             .slice(1, length - 1)
-            .map((el) => parseInt(el[4], 10)),
-        },
+            .map((el) => parseInt(el[4], 10));
+
+          stockData.xAxis = {
+            ...stockData.xAxis,
+            data: stock.slice(1, length - 1).map((el) => el[0]),
+          };
+
+          minValue = minValue
+            ? Math.min(minValue, ...priceList)
+            : Math.min(...priceList);
+          maxValue = maxValue
+            ? Math.max(maxValue, ...priceList)
+            : Math.max(...priceList);
+
+          interval = Math.floor((maxValue - minValue + 10000) / 10);
+
+          stockData.yAxis = {
+            ...stockData.yAxis,
+            min: minValue - interval < 0 ? 0 : minValue - interval,
+            max: maxValue + interval,
+            interval: interval,
+          };
+
+          stockData.series.push({
+            data: stock.slice(1, length - 1).map((el) => parseInt(el[4], 10)),
+            type: "line",
+            name: stock_list[index],
+          });
+        });
+        setLoaded(true);
+        setOption(stockData);
+        console.log(stockData);
       });
     }
     getData();
-  }, [tabIndex]);
+  }, []);
   return (
     <div className="App">
       {stock && <div>{stock.JongName}</div>}
-
-      {/*<Tabs onSelect={aa}>
-        <TabList>
-          <Tab>Title 1</Tab>
-          <Tab>Title 2</Tab>
-        </TabList>
-
-        <TabPanel>
-          {dailyPrices && (
-            <ReactEcharts ref={(e) => setChartInstance(e)} option={option} />
-          )}
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-      </Tabs>
-
-      */}
-
-      {dailyPrices && (
-        <ReactEcharts ref={(e) => setChartInstance(e)} option={option} />
+      {isLoaded && (
+        <ReactEcharts
+          style={{ height: "700px", width: "100%" }}
+          option={option}
+        />
       )}
     </div>
   );
