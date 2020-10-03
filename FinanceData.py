ccd1608113d54f84626b8468f4e6bc9d8582a8ff
Jisuk Yoon <arclien>
2020-10-03 15:54:45 +0900

    FinanceDataReader 크롤링

---
+++ FinanceData.py
@@ -0,0 +1,21 @@
+import FinanceDataReader as fdr
+# from insider import StockInsider
+import os
+
+
+
+tiker_list = ['005930', '066570', '005490'] 
+df_list = [fdr.DataReader(ticker, '2020-01-01') for ticker in tiker_list]
+
+if os.path.exists("./data")==False:
+  os.mkdir("./data")
+
+
+length = len(df_list) 
+for i in range(length):
+  df_list[i].to_csv("./data/{}.csv".format(tiker_list[i]))
+
+
+# https://tariat.tistory.com/955
+# https://github.com/charlesdong1991/StockInsider
+# https://github.com/FinanceData/FinanceDataReader
--- src/App.js
+++ src/App.js
@@ -1,20 +1,109 @@
 import React, { useEffect, useState } from "react";
 import krx from "krx-stock-api";
+import axios from "axios";
+import XMLParser from "react-xml-parser";
+import ReactEcharts from "echarts-for-react";
 import "./App.css";
+
+const getOptions = () => ({
+  tooltip: {
+    trigger: "axis",
+    axisPointer: {
+      type: "cross",
+      animation: false,
+    },
+    formatter: function (params) {
+      return `날짜 : ${params[0].name} <br> 가격 : ${params[0].value}`;
+    },
+  },
+  xAxis: {
+    type: "category",
+    data: [],
+  },
+  yAxis: {
+    type: "value",
+    axisLabel: {
+      formatter: "{value} 원",
+    },
+    min: 0,
+    max: 0,
+    interval: 0,
+  },
+  series: [
+    {
+      data: [],
+      type: "line",
+    },
+  ],
+});
 
 function App() {
   const [stock, setStock] = useState(null);
+  const [dailyPrices, setDailyPrices] = useState([]);
+  const [option, setOption] = useState(getOptions());
+
+  let xml = new XMLParser();
   useEffect(() => {
     const fetchData = async () => {
-      const res = await krx.getStock("005930");
-      console.log(res);
-      setStock(res);
+      await axios
+        .get(
+          "https://cors-anywhere.herokuapp.com/http://asp1.krx.co.kr/servlet/krx.asp.XMLSiseEng?code=005930",
+          {
+            "Content-Type": "application/xml; charset=utf-8",
+          }
+        )
+        .then((res) => {
+          const { children } = xml.parseFromString(res.data);
+          console.log(children);
+          console.log(children[0].children);
+          setStock(children[2].attributes);
+          setDailyPrices(children[0].children);
+          const data = children[0].children.map((el) =>
+            parseInt(el.attributes.day_EndPrice.replace(",", ""), 10)
+          );
+          const interval = Math.floor(
+            (Math.max(...data) - Math.min(...data) + 10000) / 5
+          );
+
+          setOption({
+            ...option,
+            xAxis: {
+              ...option.xAxis,
+              data: children[0].children
+                .map((el) => el.attributes.day_Date)
+                .reverse(),
+            },
+            yAxis: {
+              ...option.yAxis,
+              min: Math.min(...data) - interval,
+              max: Math.max(...data) + interval,
+              interval: interval,
+            },
+            series: {
+              ...option.series,
+              data: children[0].children
+                .map((el) =>
+                  parseInt(el.attributes.day_EndPrice.replace(",", ""), 10)
+                )
+                .reverse(),
+            },
+          });
+        });
     };
+    fetchData();
   }, []);
   return (
     <div className="App">
-      <header className="App-header"></header>
-      {res}
+      {stock && <div>{stock.JongName}</div>}
+      {dailyPrices &&
+        dailyPrices.map((el) => (
+          <div key={el.attributes.day_Volume}>
+            <span> {el.attributes.day_Date}</span>
+            <span> {el.attributes.day_EndPrice}</span>
+          </div>
+        ))}
+      {dailyPrices && <ReactEcharts option={option} />}
+      {option && JSON.stringify(option)}
     </div>
   );
 }
