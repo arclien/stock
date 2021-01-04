52d13150126ab9aca5d2b0735d2818607aef3d29
Arien Yuhn <aliencs@gmail.com>
2021-01-04 22:46:41 +0900

    implement STOCK-22.alert_price

--- FinanceData.py
+++ FinanceData.py
@@ -10,71 +10,76 @@ from pythonSrc.CalculateStockData import calc_stock_volume
 
 # 폴더가 없으면 만든다
 if os.path.exists(DIR) == False:
-  os.mkdir(DIR)
+    os.mkdir(DIR)
 if os.path.exists(CALC_DIR) == False:
-  os.mkdir(CALC_DIR)
+    os.mkdir(CALC_DIR)
 
 
 my_card_list = get_card_ids()
 
 for cardId in my_card_list:
- 
-  # cardId에 대한 정보를 trello에서 가져옴
-  res = get_card_by_id(cardId)
-  card_json = json.loads(res)
-  # print(card_json)
 
-  # card desc에서 정보 파싱
-  stock_name = ""
-  stock_code = ""
-  created_at = ""
-  nation = ""
-  alert_percent = ""
-  if 'desc' in card_json:
-    if card_json['desc'] != '':
-      if 'code' in json.loads(card_json["desc"]):
-        stock_code = json.loads(card_json["desc"])['code']
-      if 'name' in json.loads(card_json["desc"]):
-        stock_name = json.loads(card_json["desc"])['name']
-      if 'created_at' in json.loads(card_json["desc"]):
-        created_at = json.loads(card_json["desc"])['created_at']
-      if 'nation' in json.loads(card_json["desc"]):
-        nation = json.loads(card_json["desc"])['nation']
-      if 'alert_percent' in json.loads(card_json["desc"]):
-        alert_percent = int(json.loads(card_json["desc"])['alert_percent'])
-    else:
-      continue
+    # cardId에 대한 정보를 trello에서 가져옴
+    res = get_card_by_id(cardId)
+    card_json = json.loads(res)
+    # print(card_json)
 
-  
-  # csv 파일 매핑
-  raw_csv_file = "{}{}.csv".format(DIR, stock_code)
-  calc_csv_file = "{}{}.csv".format(CALC_DIR, stock_code)
+    # card desc에서 정보 파싱
+    stock_name = ""
+    stock_code = ""
+    created_at = ""
+    nation = ""
+    alert_percent = ""
+    alert_price = ""
+    if 'desc' in card_json:
+        if card_json['desc'] != '':
+            if 'code' in json.loads(card_json["desc"]):
+                stock_code = json.loads(card_json["desc"])['code']
+            if 'name' in json.loads(card_json["desc"]):
+                stock_name = json.loads(card_json["desc"])['name']
+            if 'created_at' in json.loads(card_json["desc"]):
+                created_at = json.loads(card_json["desc"])['created_at']
+            if 'nation' in json.loads(card_json["desc"]):
+                nation = json.loads(card_json["desc"])['nation']
+            if 'alert_percent' in json.loads(card_json["desc"]):
+                alert_percent = int(json.loads(
+                    card_json["desc"])['alert_percent'])
+            if 'alert_price' in json.loads(card_json["desc"]):
+                alert_price = int(json.loads(card_json["desc"])['alert_price'])
+        else:
+            continue
 
-  # csv 파일의 마지막 날짜로부터 다음 fetch start date를 가져옴
-  # csv 파일이 없으면 START_DATE
-  # csv 파일에 마지막 날짜가 TODAY이면 FALSE를 보내게 했음
-  fetch_start_date = get_fetch_start_date(raw_csv_file)
-  fetch_end_date = get_fetch_end_date(nation)
+    # csv 파일 매핑
+    raw_csv_file = "{}{}.csv".format(DIR, stock_code)
+    calc_csv_file = "{}{}.csv".format(CALC_DIR, stock_code)
 
-  # 이미 fetching을 했으면 fetch_start_date가 False 이다.
-  if not fetch_start_date == False:
-    fetch_and_generate_stock_csv(raw_csv_file, stock_code, fetch_start_date, fetch_end_date, nation)
-  
-  if nation == 'ko':
-    if CURRENT_TIME == AUTO_CRAWLING_TIME:
-      CRAWLING_RESULT_MSG += calc_stock_volume(raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent)
-  elif nation == 'us':
-    if CURRENT_TIME == US_CRAWLING_TIME:
-      CRAWLING_RESULT_MSG += calc_stock_volume(raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent)
+    # csv 파일의 마지막 날짜로부터 다음 fetch start date를 가져옴
+    # csv 파일이 없으면 START_DATE
+    # csv 파일에 마지막 날짜가 TODAY이면 FALSE를 보내게 했음
+    fetch_start_date = get_fetch_start_date(raw_csv_file)
+    fetch_end_date = get_fetch_end_date(nation)
+
+    # 이미 fetching을 했으면 fetch_start_date가 False 이다.
+    if not fetch_start_date == False:
+        fetch_and_generate_stock_csv(
+            raw_csv_file, stock_code, fetch_start_date, fetch_end_date, nation)
+
+    if nation == 'ko':
+        if CURRENT_TIME == AUTO_CRAWLING_TIME:
+            CRAWLING_RESULT_MSG += calc_stock_volume(
+                raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent, alert_price)
+    elif nation == 'us':
+        if CURRENT_TIME == US_CRAWLING_TIME:
+            CRAWLING_RESULT_MSG += calc_stock_volume(
+                raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent, alert_price)
 
 
 slack = Slacker(token=SLACK_TOKEN)
 
 CRAWLING_RESULT_MSG += '====================================================='
 if CURRENT_TIME == AUTO_CRAWLING_TIME or CURRENT_TIME == US_CRAWLING_TIME:
-  slack.chat.post_message(
-    channel=SLACK_CHANNEL, 
-    username=SLACK_SENDER_NAME,
-    text=CRAWLING_RESULT_MSG
-  )
-  
+    slack.chat.post_message(
+        channel=SLACK_CHANNEL,
+        username=SLACK_SENDER_NAME,
+        text=CRAWLING_RESULT_MSG
+    )
--- pythonSrc/CalculateStockData.py
+++ pythonSrc/CalculateStockData.py
@@ -6,128 +6,169 @@ import csv
 from pythonSrc.Constants import *
 from pythonSrc.Utils import *
 
-def calc_stock_volume(raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent):
-  if os.path.exists(raw_csv_file):
-    
-    # 새로운 종목의 경우 파일 만들고, headaer 생성
-    if os.path.exists(calc_csv_file) == False:
-      set_csv_header(calc_csv_file)
 
-    row_count = get_csv_row_count(raw_csv_file)
-    # 공휴일을 제외하고 VOLUME_CALC_LENGTH일 의 데이터를 가져오기 위해 csv에서 2배수로 데이터를 읽는다.
-    df = pd.read_csv(
-                  raw_csv_file, 
-                  names=["Date","Open","High","Low","Close","Volume","Change"], 
-                  skiprows = row_count - VOLUME_CALC_LENGTH[0]*2
-                )
-    
-    # 마지막 날짜(오늘)에 대한 데이터를 캐싱해둠
-    df_today = df.tail(1)
-    df_today_volume = df_today.iloc[0]['Volume']
-    df_today_price = df_today.iloc[0]['Close']
-    
-    # 오늘 날짜를 제외( 평균에서 오늘 값을 제외하기 위해서 )
-    df = df[:-1]
-    
-    # 주식시장이 열리지 않는 날은 값이 0 이므로, 해당 값이 있을 경우 drop 하고, VOLUME_CALC_LENGTH 만큼의 최신 데이터를 가져온다.
-    # 데이터 자체가, 열리지 않는 날의 값을 모두 0으로 갖고있게 되므로, 평균 계산시 영향을 끼치게 된다.
-    df = df[~(df[["Open","High","Low","Close","Volume"]] == 0).any(axis=1)]
+def calc_stock_volume(raw_csv_file, calc_csv_file, stock_code, stock_name, nation, alert_percent, alert_price):
+    if os.path.exists(raw_csv_file):
 
-    # 전날 데이터와 비교를 위해서 캐싱( 오늘이 월요일이면, 지난주 금요일로( 0의 날짜를 모두 제거 했기 때문 ))
-    df_prev_day = df.tail(1)
-    
-    calculated_row = ([TODAY])
-    alarm_message = ""
-    
-    for day in VOLUME_CALC_LENGTH:
-      df = df.tail(day)
-      alarm_message += calculate(day, df, calculated_row, df_today_volume, df_today_price, alert_percent)
+        # 새로운 종목의 경우 파일 만들고, headaer 생성
+        if os.path.exists(calc_csv_file) == False:
+            set_csv_header(calc_csv_file)
+
+        row_count = get_csv_row_count(raw_csv_file)
+        # 공휴일을 제외하고 VOLUME_CALC_LENGTH일 의 데이터를 가져오기 위해 csv에서 2배수로 데이터를 읽는다.
+        df = pd.read_csv(
+            raw_csv_file,
+            names=["Date", "Open", "High", "Low", "Close", "Volume", "Change"],
+            skiprows=row_count - VOLUME_CALC_LENGTH[0]*2
+        )
+
+        # 마지막 날짜(오늘)에 대한 데이터를 캐싱해둠
+        df_today = df.tail(1)
+        df_today_volume = df_today.iloc[0]['Volume']
+        df_today_price = df_today.iloc[0]['Close']
+
+        # 오늘 날짜를 제외( 평균에서 오늘 값을 제외하기 위해서 )
+        df = df[:-1]
+
+        # 주식시장이 열리지 않는 날은 값이 0 이므로, 해당 값이 있을 경우 drop 하고, VOLUME_CALC_LENGTH 만큼의 최신 데이터를 가져온다.
+        # 데이터 자체가, 열리지 않는 날의 값을 모두 0으로 갖고있게 되므로, 평균 계산시 영향을 끼치게 된다.
+        df = df[~(df[["Open", "High", "Low", "Close", "Volume"]] == 0).any(axis=1)]
+
+        # 전날 데이터와 비교를 위해서 캐싱( 오늘이 월요일이면, 지난주 금요일로( 0의 날짜를 모두 제거 했기 때문 ))
+        df_prev_day = df.tail(1)
+
+        calculated_row = ([TODAY])
+        alarm_message = ""
+
+        for day in VOLUME_CALC_LENGTH:
+            df = df.tail(day)
+            alarm_message += calculate(day, df, calculated_row,
+                                       df_today_volume, df_today_price, alert_percent)
+
+        # for alert_price in alert_prices:
+        alarm_message += check_alert_price(df_prev_day, df_today, alert_price)
+
+        # 파일에 데이터 추가
+        with open(calc_csv_file, "a") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(calculated_row)
+
+        if alarm_message:
+            main_link = f'> ' + '<{}|{}>'.format(
+                f'https://arclien.github.io/stock/code/{stock_code}', f'{stock_name}:{stock_code}') + f'\n'
+            additional_link = get_link_by_nation(nation, stock_code)
+            diff_symbol = "+" if df_today_price > df_prev_day.iloc[0]["Close"] else "-"
+            return f'{main_link}' + f'> `{TODAY} 거래량: {df_today_volume} / 가격: {df_today_price} ({diff_symbol}{get_diff_percent(df_prev_day.iloc[0]["Close"], df_today_price)}%)`\n' + alarm_message + f'> {additional_link}\n\n'
+        else:
+            return alarm_message
 
 
-    # 파일에 데이터 추가
-    with open(calc_csv_file, "a") as csvfile:
-      writer = csv.writer(csvfile)
-      writer.writerow(calculated_row)
-
-
-    if alarm_message:
-      main_link = f'> ' + '<{}|{}>'.format(f'https://arclien.github.io/stock/code/{stock_code}',f'{stock_name}:{stock_code}') + f'\n'
-      additional_link = get_link_by_nation(nation, stock_code)
-      diff_symbol = "+" if df_today_price > df_prev_day.iloc[0]["Close"] else "-"
-      return f'{main_link}' + f'> `{TODAY} 거래량: {df_today_volume} / 가격: {df_today_price} ({diff_symbol}{get_diff_percent(df_prev_day.iloc[0]["Close"], df_today_price)}%)`\n' + alarm_message +  f'> {additional_link}\n\n'
-    else:
-      return alarm_message
-
-  
 # 새로운 종목의 경우, 새로운 csv 파일 생성 후 header 추가
 def set_csv_header(csv_file):
-  with open(csv_file, "a") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(STOCK_CALC_LIST_HEADER)
+    with open(csv_file, "a") as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerow(STOCK_CALC_LIST_HEADER)
 
 # csv 파일을 가져와서 총 length를 계산
+
+
 def get_csv_row_count(csv_file):
-  with open(csv_file, "r") as f:
-    reader = csv.reader(f)
-    return len(list(reader))
+    with open(csv_file, "r") as f:
+        reader = csv.reader(f)
+        return len(list(reader))
+
 
 def calculate(day, df, calculated_row, df_today_volume, df_today_price, alert_percent):
-  inner_alarm_message = ""
-  _max_price = math.ceil(df.describe().loc['max']['Close'])
-  _max_volume = math.ceil(df.describe().loc['max']['Volume'])
-  _mean_volume = math.ceil(df.describe().loc['mean']['Volume'])
-  calculated_row.append(_max_volume)
-  calculated_row.append(math.ceil(df.describe().loc['min']['Volume']))
-  calculated_row.append(_mean_volume)
-  
-  # dataframe 정렬 by volume
-  df_sorted = df.sort_values(['Volume'],ascending=True)
-  # volume 기준 상/하위 5% row 지움
-  df_sorted.drop(df_sorted.tail( math.ceil(len(df) * 0.05) ).index,  inplace = True)
-  df_sorted.drop(df_sorted.head( math.ceil(len(df) * 0.05) ).index,  inplace = True)
-  # adjusted_mean
-  _adjusted_mean = math.ceil(df_sorted.describe().loc['mean']['Volume'])
-  calculated_row.append(_adjusted_mean)
+    inner_alarm_message = ""
+    _max_price = math.ceil(df.describe().loc['max']['Close'])
+    _max_volume = math.ceil(df.describe().loc['max']['Volume'])
+    _mean_volume = math.ceil(df.describe().loc['mean']['Volume'])
+    calculated_row.append(_max_volume)
+    calculated_row.append(math.ceil(df.describe().loc['min']['Volume']))
+    calculated_row.append(_mean_volume)
 
-  
-  # 최근 3일 평균을 구해야 하는데, volume이 있는 날( 주식시장 개장일 )만 평균 3일 체크
-  if not df_today_volume == 0:
-    diff_percent = get_diff_percent(_mean_volume, df_today_volume)
-    if diff_percent >= alert_percent and df_today_volume > _mean_volume:
-      inner_alarm_message += f'> {day}일 평균 대비 {diff_percent}% 증가 / '
-    
-    diff_percent = get_diff_percent(_adjusted_mean, df_today_volume)
-    if diff_percent >= alert_percent and df_today_volume > _adjusted_mean:
-      if inner_alarm_message != "":
-        inner_alarm_message += f'조정 평균 대비 {diff_percent}% 증가 / '
-      else:
-        inner_alarm_message += f'> {day}일 조정 평균 대비 {diff_percent}% 증가 / '
-      
-    if df_today_volume > _max_volume:
-      if inner_alarm_message != "":
-        inner_alarm_message += f'최대 거래량 갱신 {df_today_volume}'
-      else:
-        inner_alarm_message += f'> {day}일 최대 거래량 갱신 {df_today_volume}'
+    # dataframe 정렬 by volume
+    df_sorted = df.sort_values(['Volume'], ascending=True)
+    # volume 기준 상/하위 5% row 지움
+    df_sorted.drop(df_sorted.tail(
+        math.ceil(len(df) * 0.05)).index,  inplace=True)
+    df_sorted.drop(df_sorted.head(
+        math.ceil(len(df) * 0.05)).index,  inplace=True)
+    # adjusted_mean
+    _adjusted_mean = math.ceil(df_sorted.describe().loc['mean']['Volume'])
+    calculated_row.append(_adjusted_mean)
 
-    if inner_alarm_message:
-      inner_alarm_message += '\n'
+    # 최근 3일 평균을 구해야 하는데, volume이 있는 날( 주식시장 개장일 )만 평균 3일 체크
+    if not df_today_volume == 0:
+        diff_percent = get_diff_percent(_mean_volume, df_today_volume)
+        if diff_percent >= alert_percent and df_today_volume > _mean_volume:
+            inner_alarm_message += f'> {day}일 평균 대비 {diff_percent}% 증가 / '
+
+        diff_percent = get_diff_percent(_adjusted_mean, df_today_volume)
+        if diff_percent >= alert_percent and df_today_volume > _adjusted_mean:
+            if inner_alarm_message != "":
+                inner_alarm_message += f'조정 평균 대비 {diff_percent}% 증가 / '
+            else:
+                inner_alarm_message += f'> {day}일 조정 평균 대비 {diff_percent}% 증가 / '
+
+        if df_today_volume > _max_volume:
+            if inner_alarm_message != "":
+                inner_alarm_message += f'최대 거래량 갱신 {df_today_volume}'
+            else:
+                inner_alarm_message += f'> {day}일 최대 거래량 갱신 {df_today_volume}'
+
+        if inner_alarm_message:
+            inner_alarm_message += '\n'
+
+    if df_today_price > _max_price:
+        diff_percent = get_diff_percent(_max_price, df_today_price)
+        inner_alarm_message += f'> {day}일 최대 가격 {diff_percent}% 갱신 {_max_price} => {df_today_price} \n'
+
+    return inner_alarm_message
 
 
-  if df_today_price > _max_price:
-    diff_percent = get_diff_percent(_max_price, df_today_price)
-    inner_alarm_message += f'> {day}일 최대 가격 {diff_percent}% 갱신 {_max_price} => {df_today_price} \n'
-  
-  return inner_alarm_message
+def check_alert_price(df_yesterday, df_today, alert_price):
+    if df_today.iloc[0]['Volume'] == 0 or df_yesterday.iloc[0]['Volume'] == 0:
+        return ""
+
+    inner_alarm_message = ""
+    today_price = df_today.iloc[0]['Close']
+    today_high = df_today.iloc[0]['High']
+    today_low = df_today.iloc[0]['Low']
+    yesterday_price = df_yesterday.iloc[0]['Close']
+
+    # https://trello.com/c/Dx9Q0IOy
+    # 1. 전날 종가가 35000미만이고 오늘 종가가 35000이상이면, 35000 (돌파) (혹은 위쪽 화살표)
+    # 2. 전날 종가가 35000초과이고 오늘 종가가 35000이하이면 35000 (하향돌파) (혹은 아래쪽 화살표)
+    # 3. 전날/오늘 종가가 다 35000 초과인데, 장중 최저가가 35000이하면 35000 (지지)
+    # 4. 전날/오늘 종가가 다 35000 미만인데, 장중 최고가가 35000이상이면 35000 (저항)
+    if yesterday_price < alert_price and today_price >= alert_price:
+        inner_alarm_message += f' {alert_price} 돌파 \n'
+    elif yesterday_price > alert_price and today_price <= alert_price:
+        inner_alarm_message += f' {alert_price} 하향돌파 \n'
+    elif yesterday_price > alert_price and today_price > alert_price and today_low <= alert_price:
+        inner_alarm_message += f' {alert_price} 지지 \n'
+    elif yesterday_price < alert_price and today_price < alert_price and today_high >= alert_price:
+        inner_alarm_message += f' {alert_price} 저항 \n'
+
+    return inner_alarm_message
+
 
 def get_link_by_nation(nation, stock_code):
-  additional_link = ""
-  
-  if nation == 'ko':
-    additional_link = '<{}|{}>'.format(f'https://finance.naver.com/item/main.nhn?code={stock_code}','네이버') + f'\n'
-    additional_link += f'> ' +'<{}|{}>'.format(f'http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=701&gicode=A{stock_code}','에프엔가이드') + f'\n'
-    additional_link += f'> ' +'<{}|{}>'.format(f'https://m.comp.wisereport.co.kr:44302/CompanyInfo/Summary/{stock_code}','와이즈에프엔') + f'\n'
-  elif nation == 'us':
-    additional_link = '<{}|{}>'.format(f'https://finance.yahoo.com/quote/{stock_code}','야후') + f'\n'
-    additional_link += f'> ' +'<{}|{}>'.format(f'https://finviz.com/quote.ashx?t={stock_code}','finviz') + f'\n'
-  
-  return additional_link
+    additional_link = ""
+
+    if nation == 'ko':
+        additional_link = '<{}|{}>'.format(
+            f'https://finance.naver.com/item/main.nhn?code={stock_code}', '네이버') + f'\n'
+        additional_link += f'> ' + '<{}|{}>'.format(
+            f'http://comp.fnguide.com/SVO2/ASP/SVD_Main.asp?pGB=1&cID=&MenuYn=Y&ReportGB=&NewMenuID=11&stkGb=701&gicode=A{stock_code}', '에프엔가이드') + f'\n'
+        additional_link += f'> ' + '<{}|{}>'.format(
+            f'https://m.comp.wisereport.co.kr:44302/CompanyInfo/Summary/{stock_code}', '와이즈에프엔') + f'\n'
+    elif nation == 'us':
+        additional_link = '<{}|{}>'.format(
+            f'https://finance.yahoo.com/quote/{stock_code}', '야후') + f'\n'
+        additional_link += f'> ' + \
+            '<{}|{}>'.format(
+                f'https://finviz.com/quote.ashx?t={stock_code}', 'finviz') + f'\n'
+
+    return additional_link
