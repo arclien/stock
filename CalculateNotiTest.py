31054720adbd5587c6cb1a388a6cd0b299c7a6cf
Jisuk Yoon <thearclien@gmail.com>
2021-01-28 10:43:01 +0900

    STOCK-24 파이선 코드 multi alert price 구현

--- CalculateNotiTest.py
+++ CalculateNotiTest.py
@@ -4,7 +4,7 @@ from pythonSrc.CalculateStockData import calc_stock_volume
 
 STOCK_CODE = '005930'
 ALERT_PERCENT = 30
-ALERT_PRICE = 80000.0
+ALERT_PRICE_LIST = [80000.0, 85000.0,  86000.0, 87000.0, 88000.0, 90000.0]
 
 CRAWLING_RESULT_MSG += calc_stock_volume(
     "{}{}.csv".format(DIR, STOCK_CODE),
@@ -13,7 +13,7 @@ CRAWLING_RESULT_MSG += calc_stock_volume(
     'TEST_STOCK_NAME',
     'ko',
     ALERT_PERCENT,
-    ALERT_PRICE
+    ALERT_PRICE_LIST
 )
 
 CRAWLING_RESULT_MSG += '====================================================='
