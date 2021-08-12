bb7c8c9043a610273391af8ba0f5fe7bb44ef6ac
aliencs.mac13 <aliencs@gmail.com>
2021-08-12 12:02:53 +0900

    US_CRAWLING_TIME 07,08 둘  다  인식하게 수정

--- FinanceData.py
+++ FinanceData.py
@@ -53,7 +53,7 @@ def make_stock_dic(stock_dic):
 
 
 def update_all_stock_data(stock_dic):
-    print("update_all_stock, cur time = {}".format(CURRENT_TIME))
+    print("update_all_stock, cur time = {}({})".format(CURRENT_TIME2, CURRENT_TIME))
     count = 0
     update_count = 0
     report = ""
@@ -80,7 +80,7 @@ def update_all_stock_data(stock_dic):
 
         if stock.nation == 'ko' and CURRENT_TIME == AUTO_CRAWLING_TIME:
             report += calc_stock_volume(stock)
-        elif stock.nation == 'us'and CURRENT_TIME == US_CRAWLING_TIME:
+        elif stock.nation == 'us'and (CURRENT_TIME == US_CRAWLING_TIME or CURRENT_TIME == US_CRAWLING_TIME + 1):
             report += calc_stock_volume(stock)
         count += 1
 
--- pythonSrc/Constants.py
+++ pythonSrc/Constants.py
@@ -9,7 +9,7 @@ STOCK_LIST_HEADER = ["code","name","nation","user_id","created_at","updated_at",
 STOCK_CALC_LIST_HEADER = ['date','max_180','min_180','mean_180','adjusted_mean_180','max_90','min_90','mean_90','adjusted_mean_90','max_60','min_60','mean_60','adjusted_mean_60','max_30','min_30','mean_30','adjusted_mean_30']
 START_DATE = "2015-01-02"
 AUTO_CRAWLING_TIME = "16"
-US_CRAWLING_TIME = "08"
+US_CRAWLING_TIME = "07"
 VOLUME_CALC_LENGTH = [180,90,60,30,5]
 DATE_FORMAT = "%Y-%m-%d"
 # stock_list csv를 새롭게 업데이트 하기 위한 list
@@ -19,8 +19,9 @@ TODAY = datetime.now(timezone('Asia/Seoul')).strftime(DATE_FORMAT)
 # 16시에 git action으로 자동으로 크롤링이 돌 때에만, stock_list.csv update_at을 올려주기 위해.
 # 그 외의 시간에 updated_at을 업데이트 하면, 오후 4시에 장이 마감 후 데이터 받을 수 없는 경우가 생긴다.( updated_at이 TODAY가 아닐 경우에만 stock data fetch 하기 떄문)
 CURRENT_TIME = datetime.now(timezone('Asia/Seoul')).strftime("%H")
+CURRENT_TIME2 = datetime.now(timezone('Asia/Seoul'))
 
-CRAWLING_RESULT_MSG = f'{datetime.now()}\n크롤러 결과============================================\n\n'
+CRAWLING_RESULT_MSG = f'{CURRENT_TIME2}\n크롤러 결과============================================\n\n'
 
 # 슬랙 생성
 SLACK_TOKEN =  os.getenv('SLACK_BOT_TOKEN')
