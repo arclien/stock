2301bde57289558bbe65b7115ccb4f1aa86d8d47
Arien Yuhn <aliencs@gmail.com>
2021-09-25 14:49:25 +0900

    1. 리포트 주기 변경 2. 위클리 리포트는 5일 가격변동 기준으로 계산, 상위3개 하위3개출력하도록 수정

--- pythonSrc/StockReport.py
+++ pythonSrc/StockReport.py
@@ -6,14 +6,25 @@ def generate_stock_report(stock_dic, nation):
     print("generate stock report for {} stocks, target nation = {}".format(len(stock_dic), nation))
     stock_report = CRAWLING_RESULT_MSG
 
-    if time.localtime().tm_wday == 5 and nation == "ko":
-        stock_report += generate_weekly_summary(stock_dic, nation)
-    elif time.localtime().tm_wday == 6 and nation == "us":
-        stock_report += generate_weekly_summary(stock_dic, nation)
-        stock_report += generate_weekly_summary(stock_dic, "coin")
-    else:
+    if nation == "ko":
+        if time.localtime().tm_wday == 5:
+            stock_report += generate_weekly_summary(stock_dic, nation)
+        elif time.localtime().tm_wday == 6:
+            pass
+        else:
+            stock_report += generate_daily_summary(stock_dic, nation)
+    elif nation == "us":
+        if time.localtime().tm_wday == 6:
+            stock_report += generate_weekly_summary(stock_dic, nation)
+        elif time.localtime().tm_wday == 0:
+            pass
+        else:
+            stock_report += generate_daily_summary(stock_dic, nation)
+    else:   # case 'coin'
         stock_report += generate_daily_summary(stock_dic, nation)
-    
+        if time.localtime().tm_wday == 6:
+            stock_report += generate_weekly_summary(stock_dic, nation)
+
     return stock_report
 
 
@@ -52,21 +63,44 @@ def generate_weekly_summary(stock_dic, nation):
     up_count = 0
     even_count = 0
     down_count = 0
+    stockdiff_dic = {}
 
     for stock in stock_dic.values():
         if stock.today_data is None:
             print("{} ticker has no data".format(stock.ticker))
             continue
 
+        # 5일 값변동 찾기
+        price_diff = 0
+        for stockstat in stock.time_series:
+            if stockstat.day_range == 5:
+                price_diff = stockstat.end_price - stockstat.start_price
+
         if stock.nation == nation:
-            if stock.today_data.today_price_percent > 1.0:
+            stockdiff_dic[stock.name] = price_diff
+            if price_diff > 0:
                 up_count += 1
-            elif stock.today_data.today_price_percent < -1.0:
+            elif price_diff < 0:
                 down_count += 1
             else:
                 even_count += 1
 
-    summary += "\n> 상승종목 *{}*, 하락종목 *{}*, 횡보종목 *{}*".format(up_count, down_count, even_count)
+    sdiff_dic = sorted(stockdiff_dic.items(), reverse=True,
+                       key=lambda x: x[1])  # 오름차순 정렬
+    top3 = list(sdiff_dic)[:3]
+    bottom3 = list(sdiff_dic)[-3:]
+
+    summary += "\n> 상승종목 *{}*, 하락종목 *{}*, 횡보종목 *{}*\n".format(
+        up_count, down_count, even_count)
+
+    summary += "상승률 상위 3종목 - "
+    for item in top3:
+        summary += "{}({}) ".format(item[0], item[1])
+
+    summary += "\n상승률 하위 3종목 - "
+    for item in bottom3:
+        summary += "{}({}) ".format(item[0], item[1])
+    #summary += "{} : {}, {} : {}, {} : {}".format(top3[0][0], top3[0][1], top3[1][0], top3[1][1], top3[2][0], top3[2][1])
     return summary
 
 
--- pythonSrc/Utils.py
+++ pythonSrc/Utils.py
@@ -7,25 +7,28 @@ from pythonSrc.Constants import *
 
 # populate all date between two dates
 def daterange(start_date, end_date):
-  for n in range(int((end_date - start_date).days)+1):
-      yield start_date + timedelta(n)
+    for n in range(int((end_date - start_date).days)+1):
+        yield start_date + timedelta(n)
 
 # 해당 종목에 대한 csv 파일이 있으면 (마지막 날짜+1)을 가져오고( 이미 오늘 날짜가 있으면 FALSE 리턴 ), 아니면 START_DATE
 def get_fetch_start_date(stock_csv_file):
-  fetch_start_date = START_DATE
-  if os.path.exists(stock_csv_file):
-    with open(stock_csv_file, "r") as f:
-      # reader = csv.reader(f)
-      for content in reversed(list(csv.reader(f))):
-        if content[0] == TODAY:
-          fetch_start_date = False
-          break
-        else:
-          fetch_start_date = (datetime.strptime(content[0], DATE_FORMAT) + timedelta(days=1)).strftime(DATE_FORMAT)
-          break
-    return fetch_start_date
-  else:
-    return fetch_start_date        
+    fetch_start_date = START_DATE
+    if os.path.exists(stock_csv_file):
+        with open(stock_csv_file, "r") as f:
+            # reader = csv.reader(f)
+            for content in reversed(list(csv.reader(f))):
+                if not content:
+                    fetch_start_date = False
+                    break
+                elif content[0] == TODAY:
+                    fetch_start_date = False
+                    break
+                else:
+                    fetch_start_date = (datetime.strptime(content[0], DATE_FORMAT) + timedelta(days=1)).strftime(DATE_FORMAT)
+                    break
+        return fetch_start_date
+    else:
+        return fetch_start_date
 
 
 # KO 처음( csv 파일 없으면 )
@@ -52,22 +55,22 @@ def get_fetch_start_date(stock_csv_file):
 # 	아침 9 시가 아니면
 # 		Fetch : (마지막 날짜 + 1 ) - (Today -1)
 def get_fetch_end_date(stock):
-  prev_date = (datetime.strptime(TODAY, DATE_FORMAT) - timedelta(days=1)).strftime(DATE_FORMAT)
-  if stock.nation == 'ko':
-    if CURRENT_TIME == AUTO_CRAWLING_TIME:
-      return TODAY
-    else:
-      return prev_date
-  elif stock.nation == 'us':
-    if CURRENT_TIME == US_CRAWLING_TIME:
-      return prev_date
-    else:
-      return prev_date
-  elif stock.nation == 'coin':
-    return prev_date 
+    prev_date = (datetime.strptime(TODAY, DATE_FORMAT) - timedelta(days=1)).strftime(DATE_FORMAT)
+       if stock.nation == 'ko':
+            if CURRENT_TIME == AUTO_CRAWLING_TIME:
+                return TODAY
+            else:
+                return prev_date
+        elif stock.nation == 'us':
+            if CURRENT_TIME == US_CRAWLING_TIME:
+                return prev_date
+            else:
+                return prev_date
+        elif stock.nation == 'coin':
+            return prev_date
 
 def get_diff_percent(origin, target):
-  if target > origin:
-    return round(100 - origin / target * 100, 2)
-  else:
-    return round(100 - target / origin * 100, 2)
+    if target > origin:
+        return round(100 - origin / target * 100, 2)
+    else:
+        return round(100 - target / origin * 100, 2)
