be71e57c5add458d18578ba0619fb776cda5565b
Arien Yuhn <aliencs@gmail.com>
2022-01-13 19:34:51 +0900

    calculated 저장 제대로 안되는 부분 수정

--- Test.py
+++ Test.py
@@ -22,35 +22,22 @@ def test_push_to_slack():
     push_to_slack("test")
 
 
-def test_new_stock_report():
+def make_stock_dic_from_csv():
     stock_dic = {}
+    with open("{}stock_list.csv".format(DIR), "r") as csvfile:
+        for content in list(csv.reader(csvfile)):
+            stock = StockInfo(name=content[0], ticker=content[1],
+                        created_at=content[2], nation=content[3],
+                        alert_percent=int(content[4]), alert_prices=content[5])
+            stock_dic[content[0]] = stock
+    return stock_dic
 
-    stock1 = StockInfo(name="삼성전자", ticker="005930",
-                created_at="2020-11-20", nation="ko",
-                alert_percent=50, alert_prices="78000,80000,85000")
-
-    stock2 = StockInfo(name="케이엠더블유", ticker="032500",
-                created_at="2020-10-12", nation="ko",
-                alert_percent=50, alert_prices="49000,50000,55000,60000")
-
-    stock3 = StockInfo(name="텔라닥", ticker="TDOC",
-                created_at="2019-05-06", nation="us",
-                alert_percent=50, alert_prices="90, 100, 110, 120")
-
-    stock4 = StockInfo(name="모더나", ticker="MRNA",
-                created_at="2020-01-12", nation="us",
-                alert_percent=50, alert_prices="10,20,30,40,50,60,70")
-
-    stock_dic['삼성전자'] = stock1
-    stock_dic['케이엠더블유'] = stock2
-    stock_dic['텔라닥'] = stock3
-    stock_dic['모더나'] = stock4
-    
-    save_stock_list(stock_dic)
+def test_new_stock_report():
+    stock_dic = make_stock_dic_from_csv()
 
     #update_all_stock_data(stock_dic)
     report = generate_stock_alert_message(stock_dic)
-    report += generate_stock_report(stock_dic, "us")
+    report += generate_stock_report(stock_dic, "ko")
 
     print(report)
 
